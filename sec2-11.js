0db0be095cff4ac9d11be5e5a8b9bb7ebcabc00d
dyeyPi <jpandrey@yahoo.com>
2020-11-03 23:06:39 +0800

    Answer to Val and Vars

---
+++ sec2-11.js
@@ -0,0 +1,7 @@
+let myCountry = "Philippines";
+let myContinent = "Asia";
+let myPopulation = 100000000;
+
+console.log("My country: " + myCountry);
+console.log("My continent: " + myContinent);
+console.log("My population: " + myPopulation);
